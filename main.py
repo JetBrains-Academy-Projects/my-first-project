5a3567ac2d7b3aa37b9e1ec4dbae5de2cf203855
Ujjal Khadka <khadkauzal@gmail.com>
2023-05-23 13:00:16 +0545

    completed Stage 3/3: Net Income

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Write your code here
 print("Earned amount:")
 print("Bubblegum: $202")
 print("Toffee: $118")
@@ -5,5 +6,11 @@ print("Ice cream: $2250")
 print("Milk chocolate: $1680")
 print("Doughnut: $1075")
 print("Pancake: $80")
+income_total = 202 + 118 + 2250 + 1680 + 1075 + 80
+print(f'\nIncome: ${income_total}')
 
-print(f'\nIncome: ${202 + 118 + 2250 + 1680 + 1075 + 80}')
+expense_staff = int(input('Staff expenses:'))
+expense_other = int(input('Other expenses:'))
+
+income_net = income_total - expense_staff - expense_other
+print(f'Net income: ${income_net}')
